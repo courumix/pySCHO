9bfb152838607cafe3e5f785f0e4ec53ceaff350
Victor Contensaux <vcontensaux@poste-06.scholae.lan>
2020-11-11 11:49:26 +0100

    mise a jour perso

--- python/class_perso.py
+++ python/class_perso.py
@@ -1,2 +1,25 @@
- 
-fh
+class personnage:
+    def __init__(self,nom, pv=100):
+        
+        self._nom = nom
+        self._pv = pv_ = pv
+        self._def = 15
+        self._resi = 10
+        self._agi = 8
+        self._intel = 25
+        self._chance = 1
+        self._exp = 0
+        self._niv = 1
+        self._point_de_competence = 1
+        self._force = 10
+        self._vit = 25
+        self._mana = 50
+        
+def getNom(self):
+    return self._nom
+
+def getPv(self):
+    return self._pv
+
+def getDegats(self):
+    return self._degats
