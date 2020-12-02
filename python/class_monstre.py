08d01e8248b9c7fce796a44715132924b3a58669
Matéo Mulle <mmulle@poste-02.scholae.lan>
2020-12-02 21:22:59 +0100

    rajout de la chance et nouvelle fonction attaque dans class_monstre

--- python/class_monstre.py
+++ python/class_monstre.py
@@ -1,4 +1,4 @@
-import random # importation de plusieurs fonction utile
+import random #importation de plusieurs fonction utile
 
 
 class monstre:
@@ -6,19 +6,20 @@ class monstre:
     La class monstre sert a créer les monstres de pySCHO
     """
     def __init__(self, nom, type_monstre='Humain', pv = 10, force = 10, resistance = 10,
-                 vitesse = 10, agilite = 10, intelligence = 10, cri = 'grrrr'):
-        self._nom = str(nom) # Le nom du monstre (louis, casimir, etc...)
-        self._type = str(type_monstre) # Le type du monstre (elf, gobelins, orc, dragon, etc...)
-        self._pv = int(pv) # Le pv actuel du monstre
-        self._pv_max = int(pv) # Les pv max du monstre (utiles pour le __repr__)
-        self._force = int(force) # Les points de forces du monstre
-        self._resi = int(resistance) # Les points de résistances du monstre
-        self._vit = int(vitesse) # la vitesse du monstre
-        self._agi = int(agilite) # Les points d'agilités du monstre
-        self._intel = int(intelligence) # Les points d'intélligences du monstre
-        self._cri = str(cri) # Le cri de guerre du monstre (grrrr, gniark, etc...)
+                 vitesse = 10, agilite = 10, intelligence = 10, chance = 10, cri = 'grrrr'):
+        self._nom = str(nom) #Le nom du monstre (louis, casimir, etc...)
+        self._type = str(type_monstre) #Le type du monstre (elf, gobelins, orc, dragon, etc...)
+        self._pv = int(pv) #Le pv actuel du monstre
+        self._pv_max = int(pv) #Les pv max du monstre (utiles pour le __repr__)
+        self._force = int(force) #Les points de forces du monstre
+        self._resi = int(resistance) #Les points de résistances du monstre
+        self._vit = int(vitesse) #la vitesse du monstre
+        self._agi = int(agilite) #Les points d'agilités du monstre
+        self._intel = int(intelligence) #Les points d'intélligences du monstre
+        self._chance = int(chance) #les points de chance du monstre
+        self._cri = str(cri) #Le cri de guerre du monstre (grrrr, gniark, etc...)
         
-    # accesseurs
+    #accesseurs
     def getNom (self):
         return self._nom
     def getPv (self):
@@ -37,46 +38,38 @@ class monstre:
         return self._agi
     def getIntel (self):
         return self._intel
+    def getChance (self):
+        return self._chance
     def getCri (self):
         return self._cri    
     
-    # mutateurs
+    #mutateurs
     def setPv (self,new_pv):
         if new_pv < 0:
             self._pv = 0
         else:
             self._pv = new_pv
             
-    # méthodes
-    def att(self, perso, dmg_pv):
+    #méthodes
+    def attTmp(self, perso, dmg_pv):
         new_pv = perso.getPv() - dmg_pv
         perso.setPv(new_pv)        
-            
+    
+    def att(self, perso):
+        dmg_pv = (randint(1,self.getChance()) * self.getForce()) / perso.getResi()
+        new_pv = perso.getPv() - dmg_pv
+        perso.setPv(new_pv)
     
         
     
     
-    def __repr__(self): # Le __repr__ sert à afficher les caractéristiques du monstre
-        return self.getNom() + " est un " + str(self.getType()) + "\n PV : " + str(self.getPv()) + "/" + str(self.getPvMax()) + "\n Force : " + str(self.getForce()) + "\n Résistance : " + str(self.getResi()) + "\n vitesse : " + str(self.getVit()) + "\n Agilité : " + str(self.getAgi()) + "\n Intelligence : " + str(self.getIntel()) + "\n sont cri est : " + self.getCri()
+    def __repr__(self): #Le __repr__ sert à afficher les caractéristiques du monstre
+        return self.getNom() + " est un " + str(self.getType()) + "\n PV : " + str(self.getPv()) + "/" + str(self.getPvMax()) + "\n Force : " + str(self.getForce()) + "\n Résistance : " + str(self.getResi()) + "\n vitesse : " + str(self.getVit()) + "\n Agilité : " + str(self.getAgi()) + "\n Intelligence : " + str(self.getIntel()) + "\n Chance : " + str(self.getChance()) + "\n sont cri est : " + self.getCri()
             
             
            
            
     
   
-        
-        
-    def combat (self, perso):
-        if self.getVit > perso.getVit:
-            unite_A = self
-            unite_B = perso
-        else:
-            unite_A = perso
-            unite_B = self
-            
-        while unite_A.getPV() > 0 and unite_B.getPV()
-            print (unite_A.getNom() + "attaque" + unite_B.getNom())
-            unite_A.att(unite_B, randint(1,5) * unite_A.getForce())
-                
-            print (unite_B.getNom() + "attaque" + unite_A.getNom())
-            unite_B.att(unite_Z, randint(1,5) * unite_B.getForce())
+victor = monstre(nom="Victor", type_monstre="cobaye", pv=250, force=20, resistance=12, vitesse=5, agilite=7, intelligence=30, chance=10, cri="zzzzzzzzz")
+tobi = monstre(nom="Tobi", type_monstre="cobaye", pv=125, force=15, resistance=9, vitesse=16, agilite=17, intelligence=50, chance=15, cri="coucou")
